e949ae7308dadb7962bf86a3011415cba24aedc5
Michael Singer <michael@a-singer.de>
2019-11-28 23:53:29 +0100

    prepare for sub shells

--- gbdbg/Program.cs
+++ gbdbg/Program.cs
@@ -6,19 +6,27 @@ namespace gbdbg
 {
 	class MainClass
 	{
-		public static int Main(string[] args)
+		private static Lr35902Debugger debugger;
+
+		private static bool ValidAdrRange(Range range)
 		{
-			Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) => e.Cancel = true;
-			Console.TreatControlCAsInput = false;
+			return range != null &&
+			       range.Start >= 0 && range.Start <= 0xffff &&
+			       range.Length >= 0 && range.Length <= (0x10000 - range.Start);
+		}
 
-			Lr35902Debugger debugger = new Lr35902Debugger(args[0]);
-
-			debugger.Unlock();
+		private static int Shell(TextReader sin, TextWriter sout, TextWriter eout, bool interactive)
+		{
+			int last_error = 0;
 
 			while (true)
 			{
-				Console.Write("gbdbg# ");
-				string cmd = Console.ReadLine();
+				if (interactive)
+					sout.Write("gbdbg# ");
+				string cmd = sin.ReadLine();
+
+				if (cmd == null)
+					return last_error;
 
 				string[] a = cmd.Split(new char[] { ' ', '\r', '\n', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 				if (a.Length == 0) continue;
@@ -27,33 +35,46 @@ namespace gbdbg
 				{
 					switch (a[0])
 					{
-					case "quit":
-						return 0;
+					case "exit":
+						if (a.Length >= 2)
+						{
+							if (!NumberParser.TryParse(a[1], out last_error))
+							{
+								eout.WriteLine("Exit code not a number");
+								last_error = 1;
+							}
+						}
+						return last_error;
 					case "h":
 						debugger.Halt();
+						last_error = 0;
 						break;
 					case "c":
 						debugger.Continue();
+						last_error = 0;
 						break;
 					case "s":
 						debugger.Step();
+						last_error = 0;
 						break;
 					case "r":
 						{
 							Lr35902Registers regs = debugger.Registers;
-							Console.WriteLine(regs);
+							sout.WriteLine(regs);
 							System.IO.Stream mem = debugger.OpenMemory(regs.PC);
 							Lr35902Disassembler dis = new Lr35902Disassembler(mem);
-							Console.WriteLine("->" + mem.Position.ToString("x4") + ": " + dis.ReadLine());
-							Console.WriteLine("  " + mem.Position.ToString("x4") + ": " + dis.ReadLine());
-							Console.WriteLine("  " + mem.Position.ToString("x4") + ": " + dis.ReadLine());
+							sout.WriteLine("->" + mem.Position.ToString("x4") + ": " + dis.ReadLine());
+							sout.WriteLine("  " + mem.Position.ToString("x4") + ": " + dis.ReadLine());
+							sout.WriteLine("  " + mem.Position.ToString("x4") + ": " + dis.ReadLine());
 						}
+						last_error = 0;
 						break;
 					case "b":
 						if (a.Length != 3)
 						{
-							Console.WriteLine("Set breakpoint");
-							Console.WriteLine("Usage: b <index> <address>");
+							eout.WriteLine("Set breakpoint");
+							eout.WriteLine("Usage: b <index> <address>");
+							last_error = 2;
 							break;
 						}
 						{
@@ -61,23 +82,27 @@ namespace gbdbg
 							if (!NumberParser.TryParse(a[1], out index) ||
 								index < 0 || index >= 8)
 							{
-								Console.WriteLine("Invalid breakpoint index");
+								eout.WriteLine("Invalid breakpoint index");
+								last_error = 1;
 								break;
 							}
 							if (!NumberParser.TryParse(a[2], out address) ||
 								address < 0 || address > 0xffff)
 							{
-								Console.WriteLine("Invalid breakpoint address");
+								eout.WriteLine("Invalid breakpoint address");
+								last_error = 1;
 								break;
 							}
 							debugger.SetBreakpoint((byte)index, (ushort)address);
 						}
+						last_error = 0;
 						break;
 					case "set":
 						if (a.Length != 3)
 						{
-							Console.WriteLine("Set register");
-							Console.WriteLine("Usage: set <reg> <val>");
+							eout.WriteLine("Set register");
+							eout.WriteLine("Usage: set <reg> <val>");
+							last_error = 2;
 							break;
 						}
 						{
@@ -85,21 +110,25 @@ namespace gbdbg
 							if (!NumberParser.TryParse(a[2], out val) ||
 								val < 0 || val > 0xffff || (a[1].Length == 1 && val > 0xff))
 							{
-								Console.WriteLine("Invalid value");
+								eout.WriteLine("Invalid value");
+								last_error = 1;
 								break;
 							}
 							try { debugger.SetRegister(a[1], (ushort)val); }
 							catch(ArgumentException)
 							{
-								Console.WriteLine("Invalid register name!");
+								eout.WriteLine("Invalid register name!");
+								last_error = 1;
 							}
 						}
+						last_error = 0;
 						break;
 					case "rd":
 						if (a.Length != 2)
 						{
-							Console.WriteLine("Read byte");
-							Console.WriteLine("Usage: rd <address>");
+							eout.WriteLine("Read byte");
+							eout.WriteLine("Usage: rd <address>");
+							last_error = 2;
 							break;
 						}
 						{
@@ -107,43 +136,50 @@ namespace gbdbg
 							if (!NumberParser.TryParse(a[1], out address) ||
 								address < 0 || address > 0xffff)
 							{
-								Console.WriteLine("Invalid address");
+								eout.WriteLine("Invalid address");
+								last_error = 1;
 								break;
 							}
 							byte b = debugger.ReadMem((ushort)address);
-							Console.WriteLine("0x" + b.ToString("x2"));
+							sout.WriteLine("0x" + b.ToString("x2"));
 						}
+						last_error = 0;
 						break;
 					case "wr":
 						if (a.Length != 3)
 						{
-							Console.WriteLine("Write byte");
-							Console.WriteLine("Usage: wr <address>[+<length>] <value>");
-							Console.WriteLine("   or: wr <first>[-<last>] <value>");
+							eout.WriteLine("Write byte");
+							eout.WriteLine("Usage: wr <address>[+<length>] <value>");
+							eout.WriteLine("   or: wr <first>[-<last>] <value>");
+							last_error = 2;
 							break;
 						}
 						{
 							Range range;
 							if (!Range.TryParse(a[1], out range) || !ValidAdrRange(range))
 							{
-								Console.WriteLine("Invalid address range");
+								eout.WriteLine("Invalid address range");
+								last_error = 1;
 								break;
 							}
 							int val;
 							if (!NumberParser.TryParse(a[2], out val) ||
 								val < 0 || val > 0xff)
 							{
-								Console.WriteLine("Invalid value");
+								eout.WriteLine("Invalid value");
+								last_error = 1;
 								break;
 							}
 							debugger.WriteMemRange((ushort)range.Start, range.Length, (byte)val);
 						}
+						last_error = 0;
 						break;
 					case "xx":
 						if (a.Length > 4 || a.Length < 2)
 						{
-							Console.WriteLine("Execute instruction");
-							Console.WriteLine("Usage: xx <hex0> [<hex1> [<hex2>]]");
+							eout.WriteLine("Execute instruction");
+							eout.WriteLine("Usage: xx <hex0> [<hex1> [<hex2>]]");
+							last_error = 2;
 							break;
 						}
 						{
@@ -152,7 +188,8 @@ namespace gbdbg
 							if (!int.TryParse(a[1], NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out hex0) ||
 								hex0 < 0 || hex0 > 0xff)
 							{
-								Console.WriteLine("Invalid instruction");
+								eout.WriteLine("Invalid instruction");
+								last_error = 1;
 								break;
 							}
 							op = new byte[] { (byte)hex0 };
@@ -161,7 +198,8 @@ namespace gbdbg
 								if (!int.TryParse(a[2], NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out hex1) ||
 									hex1 < 0 || hex1 > 0xff)
 								{
-									Console.WriteLine("Invalid instruction");
+									eout.WriteLine("Invalid instruction");
+									last_error = 1;
 									break;
 								}
 								op = new byte[] { (byte)hex0, (byte)hex1 };
@@ -170,7 +208,8 @@ namespace gbdbg
 									if (!int.TryParse(a[3], NumberStyles.HexNumber, NumberFormatInfo.InvariantInfo, out hex2) ||
 										hex2 < 0 || hex2 > 0xff)
 									{
-										Console.WriteLine("Invalid instruction");
+										eout.WriteLine("Invalid instruction");
+										last_error = 1;
 										break;
 									}
 									op = new byte[] { (byte)hex0, (byte)hex1, (byte)hex2 };
@@ -178,12 +217,14 @@ namespace gbdbg
 							}
 							debugger.Execute(op);
 						}
+						last_error = 0;
 						break;
 					case "x":
 						if (a.Length < 2)
 						{
-							Console.WriteLine("Assemble and execute instruction");
-							Console.WriteLine("Usage: x <asm>");
+							eout.WriteLine("Assemble and execute instruction");
+							eout.WriteLine("Usage: x <asm>");
+							last_error = 2;
 							break;
 						}
 						{
@@ -196,35 +237,41 @@ namespace gbdbg
 							}
 							catch(AsmFormatException e)
 							{
-								Console.WriteLine(e.Message);
-								Console.WriteLine("> " + asmline);
+								eout.WriteLine(e.Message);
+								eout.WriteLine("> " + asmline);
 								for (int i = 0; i < e.Column; i++)
-									Console.Write(" ");
-								Console.WriteLine(" ^");
+									eout.Write(" ");
+								eout.WriteLine(" ^");
 								break;
 							}
 							catch
 							{
-								Console.WriteLine("Failed to assemble line");
+								eout.WriteLine("Failed to assemble line");
+								last_error = 3;
 								break;
 							}
 							m.Position = 0;
-							while (true)
+							if (interactive)
 							{
-								int b = m.ReadByte();
-								if (b == -1)
-									break;
-								Console.Write(b.ToString("X2") + " ");
+								while (true)
+								{
+									int b = m.ReadByte();
+									if (b == -1)
+										break;
+									eout.Write(b.ToString("X2") + " ");
+								}
+								eout.WriteLine();
 							}
-							Console.WriteLine();
 							debugger.Execute(m.ToArray());
 						}
+						last_error = 0;
 						break;
 					case "loadrom":
 						if (a.Length < 2)
 						{
-							Console.WriteLine("Load file into ROM while device is under reset");
-							Console.WriteLine("Usage: loadrom <path>");
+							eout.WriteLine("Load file into ROM while device is under reset");
+							eout.WriteLine("Usage: loadrom <path>");
+							last_error = 2;
 							break;
 						}
 						{
@@ -240,28 +287,32 @@ namespace gbdbg
 							}
 							catch
 							{
-								Console.WriteLine("Failed to read file \"" + path + "\"");
+								eout.WriteLine("Failed to read file \"" + path + "\"");
+								last_error = 4;
 								break;
 							}
-							Console.WriteLine("Sending file \"" + path + "\"...");
+							eout.WriteLine("Sending file \"" + path + "\"...");
 							m.Seek(0, SeekOrigin.Begin);
 							debugger.RawSend(m);
 							debugger.Unlock();
 						}
+						last_error = 0;
 						break;
 					case "dump":
 						if (a.Length != 2)
 						{
-							Console.WriteLine("Dump memory");
-							Console.WriteLine("Usage: dump <address>[+<length>]");
-							Console.WriteLine("   or: dump <first>[-<last>]");
+							eout.WriteLine("Dump memory");
+							eout.WriteLine("Usage: dump <address>[+<length>]");
+							eout.WriteLine("   or: dump <first>[-<last>]");
+							last_error = 2;
 							break;
 						}
 						{
 							Range range;
 							if (!Range.TryParse(a[1], out range, 256) || !ValidAdrRange(range))
 							{
-								Console.WriteLine("Invalid address range");
+								eout.WriteLine("Invalid address range");
+								last_error = 1;
 								break;
 							}
 							int[] b = new int[16];
@@ -269,18 +320,18 @@ namespace gbdbg
 							int len = range.Length;
 							while (len >= 0)
 							{
-								Console.Write(mem.Position.ToString("x4") + ": ");
+								sout.Write(mem.Position.ToString("x4") + ": ");
 								for (int i = 0; i < 16; i++)
 								{
 									if (i == 8)
-										Console.Write(" ");
+										sout.Write(" ");
 									b[i] = (i < len) ? mem.ReadByte() : -1;
 									if (b[i] >= 0)
-										Console.Write(" " + b[i].ToString("x2"));
+										sout.Write(" " + b[i].ToString("x2"));
 									else
-										Console.Write("   ");
+										sout.Write("   ");
 								}
-								Console.Write("  |");
+								sout.Write("  |");
 								for (int i = 0; i < 16; i++)
 								{
 									if (b[i] < 0)
@@ -289,28 +340,31 @@ namespace gbdbg
 										break;
 									}
 									if (b[i] >= 32 && b[i] < 127)
-										Console.Write((char)b[i]);
+										sout.Write((char)b[i]);
 									else
-										Console.Write(".");
+										sout.Write(".");
 								}
-								Console.WriteLine("|");
+								sout.WriteLine("|");
 								len -= 16;
 							}
 						}
+						last_error = 0;
 						break;
 					case "dis":
 						if (a.Length != 2)
 						{
-							Console.WriteLine("Disassemble memory");
-							Console.WriteLine("Usage: dis <address>[+<length>]");
-							Console.WriteLine("   or: dis <first>[-<last>]");
+							eout.WriteLine("Disassemble memory");
+							eout.WriteLine("Usage: dis <address>[+<length>]");
+							eout.WriteLine("   or: dis <first>[-<last>]");
+							last_error = 2;
 							break;
 						}
 						{
 							Range range;
 							if (!Range.TryParse(a[1], out range, 16) || !ValidAdrRange(range))
 							{
-								Console.WriteLine("Invalid address range");
+								eout.WriteLine("Invalid address range");
+								last_error = 1;
 								break;
 							}
 							System.IO.Stream mem = debugger.OpenMemory((ushort)range.Start);
@@ -319,9 +373,10 @@ namespace gbdbg
 							long len = range.Length;
 							while (mem.Position < 0xffff && mem.Position - start < len)
 							{
-								Console.WriteLine("  " + mem.Position.ToString("x4") + ": " + dis.ReadLine());
+								sout.WriteLine("  " + mem.Position.ToString("x4") + ": " + dis.ReadLine());
 							}
 						}
+						last_error = 0;
 						break;
 					case "reset":
 						if (a.Length >= 2 && a[1] == "hold")
@@ -333,35 +388,44 @@ namespace gbdbg
 							debugger.ResetTarget();
 							debugger.Unlock();
 						}
+						last_error = 0;
 						break;
 					case "unlock":
 						debugger.Unlock();
+						last_error = 0;
 						break;
 					default:
-						Console.WriteLine("Invalid command!");
+						eout.WriteLine("Invalid command!");
+						last_error = 5;
 						break;
 					}
 				}
 				catch (NotHaltedException)
 				{
-					Console.WriteLine("Target not halted!");
+					eout.WriteLine("Target not halted!");
+					last_error = 6;
 				}
 				catch (InvalidResponseException)
 				{
-					Console.WriteLine("Invalid response from target!");
+					eout.WriteLine("Invalid response from target!");
+					last_error = 7;
 				}
 				catch (EndOfStreamException)
 				{
-					Console.WriteLine("End of stream!");
+					eout.WriteLine("End of stream!");
+					last_error = 8;
 				}
 			}
 		}
 
-		private static bool ValidAdrRange(Range range)
+		public static int Main(string[] args)
 		{
-			return range != null &&
-			       range.Start >= 0 && range.Start <= 0xffff &&
-			       range.Length >= 0 && range.Length <= (0x10000 - range.Start);
+			Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) => e.Cancel = true;
+			Console.TreatControlCAsInput = false;
+
+			debugger = new Lr35902Debugger(args[0]);
+			debugger.Unlock();
+			return Shell(Console.In, Console.Out, Console.Error, true);
 		}
 	}
 }
